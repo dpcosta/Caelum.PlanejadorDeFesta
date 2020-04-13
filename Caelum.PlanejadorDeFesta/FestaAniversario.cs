24d034cea259c763e79d02e136db1f8d7f3a6767
Daniel Portugal <dpcosta@gmail.com>
2020-04-13 16:10:26 -0300

    Criação de classe para representar a festa de aniversário e seus cálculos.

---
+++ Caelum.PlanejadorDeFesta/FestaAniversario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.PlanejadorDeFesta
+{
+    public class FestaAniversario
+    {
+
+        public FestaAniversario(int qtde, string texto, bool tema)
+        {
+            QtdeConvidados = qtde;
+            TextoBolo = texto;
+            Tematica = tema;
+        }
+
+        public int QtdeConvidados { get;}
+        public string TextoBolo { get; }
+        public bool Tematica { get; }
+
+        public decimal CustoBolo
+        {
+            get
+            {
+                decimal custoLetras = TextoBolo.Length * 0.25M;
+                return QtdeConvidados >= 8 ? 75.0M + custoLetras : 40.0M + custoLetras;
+            }
+        }
+
+        public decimal CustoComida => QtdeConvidados * 25.0M;
+        public decimal CustoBebida => QtdeConvidados * 5.0M;
+        public decimal CustoTematica => Tematica ? (QtdeConvidados * 15.0M) + 50.0M : (QtdeConvidados * 7.5M) + 30.0M;
+        public decimal CustoTotal => CustoComida + CustoBebida + CustoTematica + CustoBolo;
+    }
+}
--- Caelum.PlanejadorDeFesta/FormPrincipal.cs
+++ Caelum.PlanejadorDeFesta/FormPrincipal.cs
@@ -40,23 +40,18 @@ namespace Caelum.PlanejadorDeFesta
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int qtdeConvidados = Convert.ToInt32(numConvidadosAniversario.Value);
-            string texto = textoBolo.Text;
-            bool tema = chkTemaAniversario.Checked;
-
-            decimal custoComida = qtdeConvidados * 25.0M;
-            decimal custoBebida = qtdeConvidados * 5.0M;
-            decimal custoTematica = tema ? (qtdeConvidados * 15.0M) + 50.0M : (qtdeConvidados * 7.5M) + 30.0M;
-            decimal custoLetras = texto.Length * 0.25M;
-            decimal custoBolo = qtdeConvidados >= 8 ? 75.0M + custoLetras : 40.0M + custoLetras;
-            decimal custoTotal = custoComida + custoBebida + custoTematica + custoBolo;
+            FestaAniversario aniversario = new FestaAniversario(
+                Convert.ToInt32(numConvidadosAniversario.Value), 
+                textoBolo.Text, 
+                chkTemaAniversario.Checked);
 
             string mensagem = $@"
-                O custo total da festa de aniversário R$ {custoTotal:0.00}
-                Comidas: {custoComida:0.00}
-                Bebidas: {custoBebida:0.00}
-                Temática: {custoTematica:0.00}
-                Bolo: {custoBolo:0.00}
+                O custo total da festa de aniversário R$ {aniversario.CustoTotal:0.00}
+                
+                Comidas: {aniversario.CustoComida:0.00}
+                Bebidas: {aniversario.CustoBebida:0.00}
+                Temática: {aniversario.CustoTematica:0.00}
+                Bolo: {aniversario.CustoBolo:0.00}
             ";
 
             MessageBox.Show(mensagem);
